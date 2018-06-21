cc2b7ec0b035f50c89a5624afbad573487deaddf
ookgezellig <ookgezellig@gmail.com>
2018-06-21 11:35:35 +0200

    Update SaveLeespleinToWaybackMachine.py

--- SaveLeespleinToWaybackMachine.py
+++ SaveLeespleinToWaybackMachine.py
@@ -1,15 +1,15 @@
 #! /usr/bin/env python
 
-import requests, csv
+import requests, csv, time
 
 # In : Leesplein.nl URL : e.g. https://www.leesplein.nl/jongerenliteratuurplein/auteurs-en-illustratoren/marijn-backer/215
 # Out: URL to Wayback Machine (The Internet Archive - TIA) snapshot: e.g. http://web.archive.org/web/20180607161556/https://www.leesplein.nl/jongerenliteratuurplein/auteurs-en-illustratoren/marijn-backer/215
 # Works for html pages and pdf + word files in Leesplein.nl
 
-def read_local_csv(filepath):
+def read_local_csv(filename):
     # Input=local filepath/filename of csv -- Output=List
     list = []
-    response = open(filepath, 'r')
+    response = open(filename, 'r')
     sheet = csv.reader(response, delimiter=';')
     for row in sheet:
         if not str(row).startswith('#', 2):  # skip rows in cvs that start with '#'
@@ -19,32 +19,44 @@ def read_local_csv(filepath):
 base_url = 'http://web.archive.org'
 
 urllist = []
-urllist=read_local_csv("Input-Leesplein_TeArchiverenURLs_07062018.csv") # file with 1 to-be-archived Leesplein-URL per line
-for url in urllist:
-    #print(str(url[0]))
-    r = requests.get(base_url + '/save/' + str(url[0])) #This line actually saves to URL to the Waybackmachine. All the following lines of code are for administration/display
+urllist=read_local_csv("Input-Leesplein_TeArchiverenURLs_14062018_002.txt") # file with 1 to-be-archived Leesplein-URL per line
 
-    archive_url =""
-    archive_url= r.headers['X-Cache-Key'] # httpweb.archive.org/web/20180608110403/https://www.leesplein.nl/assets/juryrapporten/2015-schaduw.pdfNL
-    if archive_url.startswith('httpweb.') and archive_url.endswith('NL'):
-        archive_url = "http://"+archive_url[4:-2] # http://web.archive.org/web/20180608110403/https://www.leesplein.nl/assets/juryrapporten/2015-schaduw.pdf
-    else: print('Unexpected value of X-Cache-Key in r.headers')
+with open("Output-Leesplein_GearchiveerdeURLs_14062018_002.txt", "w") as outputfile: # open new versin of outputfile, clear the old version
 
-    if r.status_code == requests.codes.ok:
-        contenttype = r.headers['Content-Type']
-        if "text/html" in contenttype: #html page
-            print(str(url[0])+ "^^^^" + base_url + r.headers['Content-Location']) # the "^^^^" is used a separator between the two URLs. The more traditional separators such as ";" or "," are not reliable enough, as the can occur in URLs.
-        elif contenttype == "application/pdf": #pdf file
-            print(str(url[0]) + "^^^^" + archive_url)
-        elif contenttype == "application/vnd.openxmlformats-officedocument.wordprocessingml.document": #Word doc(x)
-            print(str(url[0]) + "^^^^" + archive_url)
-        else:
-            print("Unknown Content-Type: " + str(contenttype))
-    else:
-        print('Error in response: ' + str(r.status_code))
+    for url in urllist:
+        #time.sleep(0.3)  # pause for 3 sec, not to overrequest the server
+        outputfile = open('Output-Leesplein_GearchiveerdeURLs_14062018_002.txt', 'a') # append to existing outputfile
+        linenumber = urllist.index(url)+1
+        #print(str(url[0]))
+        r = requests.get(base_url + '/save/' + str(url[0])) #This line actually saves to URL to the Waybackmachine. All the following lines of code are for administration/display
+        #print(str(r.headers))
 
-# TODO: Write everyting to a file, instead of to terminal print
+        archive_url =""
+        if r.headers['X-Cache-Key']: # bestaat niet altijd
+            archive_url= r.headers['X-Cache-Key'] # httpweb.archive.org/web/20180608110403/https://www.leesplein.nl/assets/juryrapporten/2015-schaduw.pdfNL
 
+            if archive_url.startswith('httpweb.') and archive_url.endswith('NL'):
+                archive_url = "http://"+archive_url[4:-2] # http://web.archive.org/web/20180608110403/https://www.leesplein.nl/assets/juryrapporten/2015-schaduw.pdf
+            else: print('Unexpected value of X-Cache-Key in r.headers')
+
+            if r.status_code == requests.codes.ok:
+                contenttype = r.headers['Content-Type']
+                if "text/html" in contenttype: #html page
+                    print(str(linenumber) + "^^^^" + str(url[0])+ "^^^^" + base_url + r.headers['Content-Location']) # the "^^^^" is used a separator between the two URLs. The more traditional separators such as ";" or "," are not reliable enough, as the can occur in URLs.
+                    outputfile.write(str(linenumber) + "^^^^" + str(url[0])+ "^^^^" + base_url + r.headers['Content-Location'] + "\n")
+                elif contenttype == "application/pdf": #pdf file
+                    print(str(linenumber) + "^^^^" + str(url[0]) + "^^^^" + archive_url)
+                    outputfile.write(str(linenumber) + "^^^^" + str(url[0]) + "^^^^" + archive_url + "\n")
+                elif contenttype == "application/vnd.openxmlformats-officedocument.wordprocessingml.document": #Word doc(x)
+                    print(str(linenumber) + "^^^^" + str(url[0]) + "^^^^" + archive_url)
+                    outputfile.write(str(linenumber) + "^^^^" + str(url[0]) + "^^^^" + archive_url + "\n")
+                else:
+                    print("Unknown Content-Type: " + str(contenttype))
+            else:
+                print('Error in response: ' + str(r.status_code))
+        else: print("XXXXXX - No X-Cache-Key returned for this URL")
+        outputfile.close()
+        
 #================= For reference purposes: Files type : html. pdf. docx =================
 
 #Typical r.headers snippet for html-page (better: for 'Content-Type': 'text/html;) --> 'Content-Location' is the relevant part
