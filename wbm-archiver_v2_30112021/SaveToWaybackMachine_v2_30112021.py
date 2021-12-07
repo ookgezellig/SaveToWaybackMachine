fa1d5875ceca1854be4a86d3f02c761e763a5ff8
Vera de Kok <veradekok@gmail.com>
2021-12-07 13:49:13 +0100

    Update SaveToWaybackMachine_v2_30112021.py
    clean up code

--- wbm-archiver_v2_30112021/SaveToWaybackMachine_v2_30112021.py
+++ wbm-archiver_v2_30112021/SaveToWaybackMachine_v2_30112021.py
@@ -1,4 +1,6 @@
-#! /usr/bin/env python
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
 '''
 This improved Wayback Machine Internet Archive archiving script has three modes:
 1) It can save a page/document to the Wayback Machine (WBM) of the Internet Archive (IA)
@@ -20,73 +22,62 @@ Notes:
 * .... 
 '''
 
-import csv, time
-import waybackpy
+import csv 
+import time
 from waybackpy.exceptions import WaybackError
+import waybackpy
+from numpy import loadtxt
 
-def read_local_csv(filename):
-    # Input=local filepath/filename of csv -- Output=List
-    list = []
-    response = open(filename, 'r', encoding="utf8")
-    sheet = csv.reader(response, delimiter=';')
-    for row in sheet:
-        if not str(row).startswith('#', 2):  # skip rows in cvs that start with '#'
-            list.append(row)
-    return list
-    print(list)
 
-user_agent = "Mozilla/5.0 (Windows NT 5.1; rv:40.0) Gecko/20100101 Firefox/40.0"
-infile="url-inputlist.txt" # list of urls/pages that need archiving
-outfile="url-outputlist.txt" # list of archieved urls/pages. Format = inputurl^^^^archivedurl, for example
-# https://www.kb.nl/annemarie-mol-geillustreerde-bibliografie^^^^https://web.archive.org/web/20171006113747/https://www.kb.nl/annemarie-mol-geillustreerde-bibliografie
-# This output can be easily imported into Excel using the "Data --> Text to columns" function, where the "^" must be used as separator, resulting in a 2-column Excel sheet
 
-urllist = []
-urllist=read_local_csv(infile) # file with 1 URL per line
+USER_AGENT = "Mozilla/5.0 (Windows NT 5.1; rv:40.0) Gecko/20100101 Firefox/40.0"
+INFILE="url-inputlist.csv" # list of urls/pages that need archiving
+OUTFILE="url-outputlist.csv" # list of archieved urls/pages. 
 
-with open(outfile, "w") as outputfile: # open new version of outputfile, clear the old version
+csvfile =  open(OUTFILE , "w", newline='\n', encoding='utf-8')
+spamwriter = csv.writer(csvfile, dialect='excel', delimiter=';', quotechar='"', quoting=csv.QUOTE_MINIMAL)
 
-    for url in urllist:
-        time.sleep(1)  # pause for 1 sec, not to overrequest the server.
+
+urllist = loadtxt(INFILE, dtype=str, comments="#", delimiter="\n", encoding='utf-8')
+for url in urllist:
+    time.sleep(1)  # pause for 1 sec, not to overrequest the server.
 # Choose longer times for archiving pdf and doc files, as the archiving software needs more time to read these docs than standard html pages
-        outputfile = open(outfile, 'a') # append to existing outputfile
-        #linenumber = urllist.index(url)+1
-        wayback = waybackpy.Url(url[0], user_agent)
+    
+
+    wayback = waybackpy.Url(url, USER_AGENT)
 
 # MODE 1: Code for saving url/page
-        try:
-            archive = wayback.save()
-            print(str(url[0] + "^^^^" + str(archive.archive_url)))
-            outputfile.write(str(url[0] + "^^^^" + str(archive.archive_url))+"\n")
-        except WaybackError as e:
-            print(str(url[0] + "^^^^" + "WaybackError - could not save page to WBM"))
-            outputfile.write(str(url[0] + "^^^^" + "WaybackError - could not save page to IA")+"\n")
-        except AttributeError as d:
-            print(str(url[0] + "^^^^" + "AttributeError - could not save page to IA"))
-            outputfile.write(str(url[0] + "^^^^" + "AttributeError - could not save page to WBM")+"\n")
+    try:
+        archive = wayback.save()
+        print(url,  archive.archive_url)
+        spamwriter.writerow([url, archive.archive_url])
+    except WaybackError as e:
+        print(url,  "WaybackError - could not save page to WBM")
+        spamwriter.writerow([url , "WaybackError - could not save page to IA"])
+    except AttributeError as d:
+        print(url,  "AttributeError - could not save page to IA")
+        spamwriter.writerow([url, "AttributeError - could not save page to WBM"])
 
 # MODE 2: Code for retrieving latest/newest version of page
-#         try:
-#             archive = wayback.newest()
-#             print(str(url[0] + "^^^^" + str(archive.archive_url)))
-#             outputfile.write(str(url[0] + "^^^^" + str(archive.archive_url))+"\n")
-#         except WaybackError as e:
-#             print(str(url[0] + "^^^^" + "WaybackError - page not archived in WBM"))
-#             outputfile.write(str(url[0] + "^^^^" + "WaybackError - page not archived in WBM")+"\n")
-#         except AttributeError as d:
-#             print(str(url[0] + "^^^^" + "AttributeError - page not archived in WBM"))
-#             outputfile.write(str(url[0] + "^^^^" + "AttributeError - page not archived in WBM")+"\n")
+#   try:
+#       archive = wayback.newest()
+#       print(url,  archive.archive_url)
+#       spamwriter.writerow([url,  archive.archive_url)])
+#   except WaybackError as e:
+#       print(url,  "WaybackError - page not archived in WBM")
+#       spamwriter.writerow([url,  "WaybackError - page not archived in WBM"])
+#   except AttributeError as d:
+#       print(url,  "AttributeError - page not archived in WBM")
+#       spamwriter.writerow([url,  "AttributeError - page not archived in WBM"])
 
 # MODE 3: Code for retrieving oldest/first version of url/page 
-#         try:
-#             archive = wayback.oldest()
-#             print(str(url[0] + "^^^^" + str(archive.archive_url)))
-#             outputfile.write(str(url[0] + "^^^^" + str(archive.archive_url))+"\n")
-#         except WaybackError as e:
-#             print(str(url[0] + "^^^^" + "WaybackError - page not archived in WBM"))
-#             outputfile.write(str(url[0] + "^^^^" + "WaybackError - page not archived in WBM")+"\n")
-#         except AttributeError as d:
-#             print(str(url[0] + "^^^^" + "AttributeError - page not archived in WBM"))
-#             outputfile.write(str(url[0] + "^^^^" + "AttributeError - page not archived in WBM")+"\n")
-    outputfile.close()
-
+#  try:
+#      archive = wayback.oldest()
+#      print(url,  archive.archive_url)))
+#      spamwriter.writerow([url,  archive.archive_url])
+#  except WaybackError as e:
+#      print(url,  "WaybackError - page not archived in WBM")
+#      spamwriter.writerow([url,  "WaybackError - page not archived in WBM"])
+#  except AttributeError as d:
+#      print(url,  "AttributeError - page not archived in WBM")
+#      spamwriter.writerow([url,  "AttributeError - page not archived in WBM"])
